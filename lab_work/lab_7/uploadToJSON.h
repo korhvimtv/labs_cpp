49f7bc5c0ec67fe09de4bbd17ac71661dd16e162
korhvimtv <mt.korhovoi@gmail.com>
2024-11-29 01:36:44 +0300

    delete directory

--- lab_work/lab_7/uploadToJSON.h
+++
@@ -1,9 +0,0 @@
-#ifndef LAB_WORK_UPLOADTOJSON_H
-#define LAB_WORK_UPLOADTOJSON_H
-
-
-#include "../libs.h"
-
-void saveStudentsToJson(const std::vector<Student>& students, const std::string& filename);
-
-#endif //LAB_WORK_UPLOADTOJSON_H
