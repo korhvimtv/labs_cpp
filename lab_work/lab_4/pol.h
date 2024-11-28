077d69a220cd7c7f8e041285f08514f8a88df789
korhvimtv <mt.korhovoi@gmail.com>
2024-11-29 01:17:22 +0300

    lab 3 - 7

---
+++ lab_work/lab_4/pol.h
@@ -0,0 +1,8 @@
+#ifndef LAB_WORK_POL_H
+#define LAB_WORK_POL_H
+
+#include "../libs.h"
+
+void countUsersInOrganisation(const std::vector<InfoProvider*>& users, const std::string& organisation);
+
+#endif //LAB_WORK_POL_H
