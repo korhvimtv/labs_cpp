1137406e17ff537a3853a42801cbda11df7c6807
korhvimtv <mt.korhovoi@gmail.com>
2024-11-29 01:40:31 +0300

    delete directory

--- lab_work/lab_4/pol.h
+++
@@ -1,8 +0,0 @@
-#ifndef LAB_WORK_POL_H
-#define LAB_WORK_POL_H
-
-#include "../libs.h"
-
-void countUsersInOrganisation(const std::vector<InfoProvider*>& users, const std::string& organisation);
-
-#endif //LAB_WORK_POL_H
